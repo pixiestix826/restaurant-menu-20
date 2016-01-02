391caff8cbfc7e24feb06db328458055cbf467cc
Brandi Soucy <bbsoucy@gmail.com>
2016-01-02 15:33:26 -0600

    adding edit simple form.

--- app/router.js
+++ app/router.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
-  location: config.locationType
+  location: config.locationType,
 });
 
 Router.map(function() {
@@ -16,7 +16,7 @@ Router.map(function() {
   });
 
   this.route('menu', {
-    path: '/'
+    path: '/',
   });
 });
 
